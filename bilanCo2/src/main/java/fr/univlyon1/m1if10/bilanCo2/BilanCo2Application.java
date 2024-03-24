9f42614cd6ccb382871ca047cff0c8622c9944ee
Valentin Cuzin-Rambaud <valentincuzin@murena.io>
2024-03-24 23:00:00 +0100

    Ajout des checkstyles, JwtHelper, WebConfig ->json
    Il reste à faire le liens avec la base de données postgresql, créer le
    model en accord avec la structure du sql, faires les repositories puis
    les controllers.
    On branch Spring_boot_begin
    Changes to be committed:
    	new file:   bilanCo2/checkstyle.xml
    	modified:   bilanCo2/pom.xml
    	modified:   bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/BilanCo2Application.java
    	new file:   bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/WebConfig.java
    	new file:   bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/utils/JwtHelper.java

--- bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/BilanCo2Application.java
+++ bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/BilanCo2Application.java
@@ -2,8 +2,10 @@ package fr.univlyon1.m1if10.bilanCo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebConfig.class)
 public class BilanCo2Application {
 
 	public static void main(String[] args) {
---
+++ bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/WebConfig.java
@@ -0,0 +1,13 @@
+package fr.univlyon1.m1if10.bilanCo2;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
