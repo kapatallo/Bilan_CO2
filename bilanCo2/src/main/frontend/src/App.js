a94487982e56c12e7a8750011a9a0fc8ac89242f
jawad mecheri <jmecheri01@yahoo.com>
2024-03-30 19:09:39 +0000

    correction des erreurs de chemin

--- bilanCo2/src/main/frontend/src/App.js
+++ bilanCo2/src/main/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterForm from './Pages/RegisterPage/RegisterForm.jsx';
-import DashboardPage from './DashboardPage/DashboardPage.jsx';
+import DashboardPage from './Pages/DashboardPage/DashboardPage.jsx';
 import SettingPage from './Pages/SettingPage/SettingPage.jsx';
 import ComparisonPage from './Pages/ComparisonPage/ComparisonPage.jsx';
 import './App.css';
