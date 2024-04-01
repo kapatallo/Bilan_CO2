ac85b1c0b72eb0eb82426eaacc901dd4b970d1bd
jawad mecheri <jmecheri01@yahoo.com>
2024-04-01 04:41:19 +0000

    ajout graphe d'emission + correction de l'affichage de la navBar et infoBar

--- bilanCo2/src/main/frontend/src/Pages/DashboardPage/DashboardPage.jsx
+++ bilanCo2/src/main/frontend/src/Pages/DashboardPage/DashboardPage.jsx
@@ -6,6 +6,7 @@ import InfoCard from "./InfoCard";
 import CarbonIcon from "../../resources/icons/carbon.svg";
 import FeetIcon from "../../resources/icons/feet.svg";
 import EcoIcon from "../../resources/icons/eco.svg";
+import LineChartComponent from './LineChartComponent';
 
 
 
@@ -15,35 +16,42 @@ export default function DashboardPage() {
     const carboneEmit= 434 ;
     const co2Km = 2; 
     const choix = 80;
+    const data_chart = [100, 400, 300, 800, 400, 200, 400];
 
     return (
         <div className="dashboard-page">
             <NavBar />
             <div className="dashboard-content">
-            <p className="header-page">Pages / <span className="header-page-name">Dashboard</span></p>
-            <h2 className="page-title">Dashboard</h2>
+                <p className="header-page">Pages / <span className="header-page-name">Dashboard</span></p>
+                <h2 className="page-title">Dashboard</h2>
                 <WelcomeCard username={username} top={top} />
                 <div className="dashboard-stats">
-                    <InfoCard
-                        icon={CarbonIcon}
-                        value= {carboneEmit}
-                        unit="kgs"
-                        label="De carbone émit ce mois-ci"
-                    />
-                    <InfoCard
-                        icon={FeetIcon}
-                        value={co2Km}
-                        unit="Kgs/100km"
-                        label="De CO2 moyennes par kilomètres parcouru"
-                    />
-                    <InfoCard
-                        icon={EcoIcon}
-                        value={choix}
-                        unit="%"
-                        label="De choix éco-friendly"
-                    />
-                    </div>
+                        <InfoCard
+                            icon={CarbonIcon}
+                            value= {carboneEmit}
+                            unit="kgs"
+                            label="De carbone émit ce mois-ci"
+                        />
+                        <InfoCard
+                            icon={FeetIcon}
+                            value={co2Km}
+                            unit="Kgs/100km"
+                            label="De CO2 moyennes par kilomètres parcouru"
+                        />
+                        <InfoCard
+                            icon={EcoIcon}
+                            value={choix}
+                            unit="%"
+                            label="De choix éco-friendly"
+                        />
                 </div>
+                <h2 className="emission-history">Historique d’émission</h2>
+                <div className="chart-container">
+                <LineChartComponent dataChart={data_chart} />
+                </div>
+                
+
+            </div>
             <div className="dashboard-info">
 
             </div>
---
+++ bilanCo2/src/main/frontend/src/Pages/DashboardPage/LineChartComponent.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useRef }  from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+export default function LineChartComponent({ dataChart }) {
+    const chartRef = useRef();
+
+    useEffect(() => {
+        if (chartRef.current) {
+            const chart = chartRef.current;
+            const ctx = chart.canvas.getContext('2d');
+            const gradient = ctx.createLinearGradient(0, 0, 0, chart.canvas.offsetHeight);
+            gradient.addColorStop(0, 'rgba(45, 96, 255, 0.4)'); 
+            gradient.addColorStop(0.8, 'rgba(45, 96, 255, 0)');
+            chart.data.datasets[0].backgroundColor = gradient;
+            chart.update();
+        }
+    }, [dataChart]);
+
+    const data = {
+        labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan'],
+        datasets: [
+            {
+                label: '',
+                data: dataChart,
+                fill: true,
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                borderColor: '#1814F3',
+                pointBorderColor: '#303f9f',
+                pointBackgroundColor: '#ffffff',
+                pointHoverBackgroundColor: '#303f9f',
+                pointHoverBorderColor: '#ffffff',
+                tension: 0.4,
+                pointBorderWidth: 2,
+                pointRadius: 4,
+                pointHoverRadius: 6
+            },
+        ],
+    };
+
+    const options = {
+        plugins: {
+            legend: {
+                display: false 
+            },
+            tooltip: {
+                enabled: true 
+            }
+        },
+        scales: {
+        y: {
+            beginAtZero: true,
+            ticks: {
+                // Définir manuellement les valeurs de tick
+                callback: function(value, index, values) {
+                    const allowedValues = [0, 200, 400, 600, 800];
+                    if (allowedValues.includes(value)) {
+                        return value;
+                    }
+                    return null;
+                },
+                color: '#9e9e9e', 
+                stepSize: 200, 
+                min: 0,
+                max: 800
+            },
+            grid: {
+                drawBorder: false,
+                color: function(context) {
+                    if (context.tick.value === 0)
+                        return 'rgba(0, 0, 0, 0)'; 
+                    return 'rgba(0, 0, 0, 0.1)';
+                }
+            },
+        },
+            x: {
+                grid: {
+                    display: false, // Cacher les lignes de grille de l'axe X
+                },
+                ticks: {
+                    color: '#9e9e9e' // Couleur des étiquettes de l'axe X
+                }
+            }
+        }
+    };
+
+    return <Line ref={chartRef} data={data} options={options} style={{ margin:'40px' }}/>;
+}
