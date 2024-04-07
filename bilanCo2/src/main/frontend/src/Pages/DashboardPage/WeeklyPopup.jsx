2c9576349a41530bd2c8d73382179f6e9e1dd82b
jawad mecheri <jmecheri01@yahoo.com>
2024-04-07 03:09:56 +0000

    Mise en place initial du composant 'Form' dans la popup (pas encore adapté au questions hebdo).

--- bilanCo2/src/main/frontend/src/Pages/DashboardPage/WeeklyPopup.jsx
+++ bilanCo2/src/main/frontend/src/Pages/DashboardPage/WeeklyPopup.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import './WeeklyPopup.css'; 
-
+import FormQuestions from "./../Form/FormQuestions";
 export default function WeeklyPopup({ show, onClose }) {
+  
+  const questions = require('./../FirstFormPage/questions.json');
+  const [formPart, setFormPart] = useState({
+    "partText":"transport",
+    "partId":0
+});
+
     if (!show) {
       return null;
     }
-  
     return (
       <div className="popup-overlay">
         <div className="popup">
           <button className="close-btn" onClick={onClose}>×</button>
-            helloooooo
+          <FormQuestions data={questions} formId={formPart.partId} />
         </div>
       </div>
     );
