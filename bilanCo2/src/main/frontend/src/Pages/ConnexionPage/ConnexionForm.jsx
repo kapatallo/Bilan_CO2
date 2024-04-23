858e13647e8a89b483b72fee63c4b03688fb18d8
Sorbexos <sorbexos@gmail.com>
2024-04-23 12:42:10 +0200

    rajout des requêtes post pour inscriptions et connexion

--- bilanCo2/src/main/frontend/src/Pages/ConnexionPage/ConnexionForm.jsx
+++ bilanCo2/src/main/frontend/src/Pages/ConnexionPage/ConnexionForm.jsx
@@ -1,14 +1,28 @@
 // ConnectionForm.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ConnexionForm.css'; 
 import LogoName from '../../resources/logo/CarbonPrint.svg';
 import Logo from '../../resources/logo/Logo.svg';
 
-export default function RegisterForm() {
+export default function ConnexionForm() {
     
     let navigate = useNavigate();
+
+    const [connexionData, setConnexionData] = useState({
+        "email":null,
+        "mdp":null
+    })
+
+    useEffect( () => console.log(connexionData),[connexionData]);
     
+    const handleChange = (event) => {
+        setConnexionData({
+            ...connexionData,
+            [event.target.id]: event.target.value
+        });
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         // Logique de validation du formulaire
@@ -16,20 +30,20 @@ export default function RegisterForm() {
     }
 
     return (
-        <div className="register-form-container">
+        <div className="connexion-form-container">
             <img src={Logo} alt="CarbonPrint logo" className="logo" />
             <img src={LogoName} alt="CarbonPrint" className="logoName" />
             
-            <form onSubmit={handleSubmit} className="register-form">
+            <form onSubmit={handleSubmit} className="connexion-form">
 
                 <div className="input-container">
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required/>
+                    <input onChange={handleChange} type="email" id="email" name="email" required/>
                 </div>
 
                 <div className="input-container">
                     <label htmlFor="pwd">Mot de passe:</label>
-                    <input type="password" id="pwd" name="pwd" required/>
+                    <input onChange={handleChange} type="password" id="pwd" name="pwd" required/>
                 </div>
 
                 <div className="input-container">
