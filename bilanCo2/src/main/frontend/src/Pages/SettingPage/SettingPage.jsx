5c2a9a74d5e44e65f91e3d01c7af44c51209b8af
Eragon6 <119508559+Eragon6@users.noreply.github.com>
2024-04-24 13:55:42 +0200

    Ajout de la page des Setting(Paramètres)

--- bilanCo2/src/main/frontend/src/Pages/SettingPage/SettingPage.jsx
+++ bilanCo2/src/main/frontend/src/Pages/SettingPage/SettingPage.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import NavBar from '../../NavBar/NavBar';
+import { useState } from 'react';
+import { Form } from 'react-router-dom';
+import './SettingsPage.css'
 
 export default function SettingPage() {
+    const [inputValue, setValue] = useState('');
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setValue(values => ({ ...values, [name]: value }));
+    }
     return (
         <div>
-        <NavBar />
-      </div>
-          )
+            <h1>Paramètres</h1>
+            <h2>Vos informations</h2>
+            <form>
+                <ul className='settings-container'>
+                    <li>
+                        <label>
+                            <span>Nom :</span>
+                        </label>
+                        <input type="text" name="name" value={inputValue.name} onChange={handleChange} />
+                    </li>
+                    <li>
+                        <label>
+                            <span>Prénom :</span>
+                        </label>
+                        <input type="text" name="firstnmame" value={inputValue.firstnmame} onChange={handleChange} />
+                    </li>
+                    <li>
+                        <label>
+                            <span>E-mail :</span>
+                        </label>
+                        <input type="email" name="email" value={inputValue.email} onChange={handleChange} />
+                    </li>
+                    <li>
+                        <label>
+                            <span>Mot de passe :</span>
+                        </label>
+                        <input type="password" name="mdp" value={inputValue.mdp} onChange={handleChange} />
+                    </li>
+                    <li>
+                        <label>
+                            <span>Confirmer le mot de passe :</span>
+                        </label>
+                        <input type="password" name="confmdp" value={inputValue.confmdp} onChange={handleChange} />
+                    </li>
+                </ul>
+                <div className="input-container-button">
+                    <input type="submit" value="Modifier" />
+                    <input type="submit" value="Annuler" />
+                </div>
+
+            </form>
+        </div>
+    )
 }
