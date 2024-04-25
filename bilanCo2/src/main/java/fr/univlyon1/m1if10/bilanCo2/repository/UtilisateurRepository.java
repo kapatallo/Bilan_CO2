8577b2283a0db2670af4ab35061035788ab62100
Mathys Sambet <mathys.sambet@etu.univ-lyon.fr>
2024-04-25 19:05:55 +0200

    ajout d'une requete pour trouver les utilisateurs par mail et mdp

--- bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/repository/UtilisateurRepository.java
+++ bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/repository/UtilisateurRepository.java
@@ -3,11 +3,18 @@ package fr.univlyon1.m1if10.bilanCo2.repository;
 import fr.univlyon1.m1if10.bilanCo2.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * The interface Utilisateur repository.
  */
 @Component
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+
+    @Query("SELECT U FROM Utilisateur U WHERE U.email=:email AND U.mdp=:mdp")
+    Optional<Utilisateur> findByMailMdp(@Param("email") String email, @Param("mdp") String mdp);
 }
