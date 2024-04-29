fd306518ebd2e713f8976a0f09e1a2644485f2d4
Cuzin-Rambaud <valentincuzin@murena.io>
2024-04-30 00:47:34 +0200

    add cors to repository

--- bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/repository/TransportRepository.java
+++ bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/repository/TransportRepository.java
@@ -5,12 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
 
 /**
  * The interface Transport repository.
  */
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000", "http://localhost",
+                        "https://192.168.75.51/api", "http://192.168.75.51",
+                        "https://192.168.75.51"})
 @Component
 public interface TransportRepository  extends JpaRepository<Transport, Long> {
 
