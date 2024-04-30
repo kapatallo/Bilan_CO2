95ab3c624efbb908406b434bdd77ea35fc495d5d
Cuzin-Rambaud <valentincuzin@murena.io>
2024-04-30 11:05:33 +0200

    cors

--- bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/WebConfig.java
+++ bilanCo2/src/main/java/fr/univlyon1/m1if10/bilanCo2/WebConfig.java
@@ -2,6 +2,7 @@ package fr.univlyon1.m1if10.bilanCo2;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -12,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
